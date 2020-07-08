f1ca5fd6683b9ff78b434389d8c5c91b1cc5d4d7
hakeesh <67994588+hakeesh@users.noreply.github.com>
2020-07-08 15:52:34 +1000

    Update init.py
    initial init.py file

--- init.py
+++ init.py
@@ -1,1 +1,10 @@
+# --------------------------------------------------------------
+#  init.py
+#  Version: 1.0.0
+#  Last Updated: July 8th, 2020
+# --------------------------------------------------------------
 
+# ----- DEFINE CUSTOM FOLDER STRUCTURE -------------------------
+	
+nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./python')
