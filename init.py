79d0969053ddd46b5a30060eb1aa8d0f441caa86
hakeesh <67994588+hakeesh@users.noreply.github.com>
2020-07-21 15:14:50 +1000

    Update init.py

--- init.py
+++ init.py
@@ -1,11 +1,12 @@
 # --------------------------------------------------------------
 #  init.py
-#  Version: 1.0.0
-#  Last Updated: July 8th, 2020
+#  Version: 1.0.1
+#  Last Updated: July 21th, 2020
 # --------------------------------------------------------------
 
 # ----- DEFINE CUSTOM FOLDER STRUCTURE -------------------------
 	
 nuke.pluginAddPath('./gizmos')
 nuke.pluginAddPath('./python')
+nuke.pluginAddPath('./python/shuffleShortcuts')
 nuke.pluginAddPath('./icons')
