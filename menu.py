a9716ba8f6539e7f01afda459d5582ed0fafb27b
hakeesh <67994588+hakeesh@users.noreply.github.com>
2020-07-22 16:45:56 +1000

    Update menu.py

--- menu.py
+++ menu.py
@@ -70,7 +70,6 @@ nuke.menu('Nodes').addCommand("Merge/Premult", "nuke.createNode('Premult')", "sh
 
 import shuffleShortcuts
 import listNavigator
-import filepathLister
 import paste_selected
 
 # ----- MERGE NODE SHORTCUTS -----------------------------------
